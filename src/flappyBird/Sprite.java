970eae6774a639c80e6c4f7194a8e2bfa22871c3
zjy555666 <57013433+yung555666@users.noreply.github.com>
2021-06-07 13:31:45 +0800

    创建动画类,基本的游戏已完成

---
+++ src/flappyBird/Sprite.java
@@ -0,0 +1,86 @@
+package flappyBird;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+
+public class Sprite {
+    private Image image;
+    private double positionX;
+    private double positionY;
+    private double velocityX;
+    private double velocityY;
+    private double width;
+    private double height;
+
+    public Sprite() {
+        this.positionX = 0;
+        this.positionY = 0;
+        this.velocityX = 0;
+        this.velocityY = 0;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+        this.width = image.getWidth();
+        this.height = image.getHeight();
+    }
+
+    public void resizeImage(String filepath, int width, int height) {
+        Image toReturn = new Image(filepath, width, height, false, false);
+        setImage(toReturn);
+    }
+
+    public void setPositionXY(double positionX, double positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public double getPositionX() {
+        return positionX;
+    }
+
+    public double getPositionY() {
+        return positionY;
+    }
+
+    public void setVelocity(double velocityX, double velocityY) {
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+    }
+
+    public void addVelocity(double x, double y) {
+        this.velocityX += x;
+        this.velocityY += y;
+    }
+
+    public double getVelocityX() {
+        return velocityX;
+    }
+
+    public double getVelocityY() {
+        return velocityY;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void render(GraphicsContext gc) {
+        gc.drawImage(image, positionX, positionY);
+    }
+
+    public Rectangle2D getBoundary() {
+        return new Rectangle2D(positionX, positionY, width, height);
+    }
+
+    public boolean intersectsSprite(Sprite s) {
+        return s.getBoundary().intersects(this.getBoundary());
+    }
+
+    public void update(double time) {
+        positionX += velocityX * time;
+        positionY += velocityY * time;
+    }
+}
