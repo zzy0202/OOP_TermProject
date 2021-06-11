6bb69db00ddfe095e539433cefa1c9c56b76658d
shiran2333 <70182211+shiran2333@users.noreply.github.com>
2021-06-11 17:24:07 +0800

    添加了javadoc注释

--- src/spaceShip/SpaceShipButton.java
+++ src/spaceShip/SpaceShipButton.java
@@ -8,11 +8,18 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Font;
 
+/**
+ * 这个方法主要是用来设置游戏中的各种按钮，这里利用了extend java自带的Button类
+ */
 public class SpaceShipButton extends Button {
     private final String FONT_PATH = "/resources/kenvector_future.ttf";
     private final String BUTTON_PRESSED_STYLE = "-fx-background-color: transparent; -fx-background-image: url('/resources/red_button_pressed.png');";
     private final String BUTTON_FREE_STYLE = "-fx-background-color: transparent; -fx-background-image: url('/resources/red_button.png');";
 
+    /**
+     * 先设置button的规格
+     * @param text
+     */
     public SpaceShipButton(String text) {
         this.setText(text);
         this.setButtonFont();
@@ -22,22 +29,33 @@ public class SpaceShipButton extends Button {
         this.initializeButtonListeners();
     }
 
+    /**
+     * 设置字体的样式和大小
+     */
     private void setButtonFont() {
         this.setFont(Font.loadFont(this.getClass().getResourceAsStream("/resources/kenvector_future.ttf"), 23.0D));
     }
 
+    /**
+     * 设置按下按钮的效果（这里直接利用图片）
+     */
     private void setButtonPressedStyle() {
         this.setStyle("-fx-background-color: transparent; -fx-background-image: url('/resources/red_button_pressed.png');");
         this.setPrefHeight(45.0D);
         this.setLayoutY(this.getLayoutY() + 4.0D);
     }
-
+    /**
+     * 设置按钮的效果（这里直接利用图片）
+     */
     private void setButtonReleasedStyle() {
         this.setStyle("-fx-background-color: transparent; -fx-background-image: url('/resources/red_button.png');");
         this.setPrefHeight(45.0D);
         this.setLayoutY(this.getLayoutY() - 4.0D);
     }
 
+    /**
+     * 设置游戏里的按钮
+     */
     private void initializeButtonListeners() {
         this.setOnMousePressed(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent event) {
