90bde1be22153d6fcf8f42a1bec51ab60899585d
shiran2333 <70182211+shiran2333@users.noreply.github.com>
2021-06-11 17:23:44 +0800

    添加了javadoc注释

--- src/spaceShip/SmallInfoLabel.java
+++ src/spaceShip/SmallInfoLabel.java
@@ -10,10 +10,16 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.text.Font;
-
+/**
+ * @author shiran
+ * 这个类主要是设置图标背景的图片以及字体
+ */
 public class SmallInfoLabel extends Label {
     private static final String FONT_PATH = "/resources/kenvector_future.ttf";
-
+    /**
+     * 这个方法传入文字之后，按照预定的设置生成。
+     * @param text
+     */
     public SmallInfoLabel(String text) {
         this.setPrefWidth(130.0D);
         this.setPrefHeight(50.0D);
@@ -25,6 +31,9 @@ public class SmallInfoLabel extends Label {
         this.setText(text);
     }
 
+    /**
+     * 设置字体样式以及大小
+     */
     private void setLabelFont() {
         this.setFont(Font.loadFont(this.getClass().getResourceAsStream("/resources/kenvector_future.ttf"), 15.0D));
     }
